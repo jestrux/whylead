39f9532cc747cd5bb59afdea0eb071e6cbc69b97
Walter Kimaro <wakyj07@gmail.com>
2024-03-27 13:04:22 +0300

    Use question mark shape for popups

--- resources/views/home/challenges.blade.php
+++ resources/views/home/challenges.blade.php
@@ -158,7 +158,8 @@
                             </p>
 
                             <div class="my-6">
-                                <h5 class="text-accent dark:text-content/70 text-sm/none uppercase font-bold tracking-wide mb-5">
+                                <h5
+                                    class="text-accent dark:text-content/70 text-sm/none uppercase font-bold tracking-wide mb-5">
                                     Recommended solutions
                                 </h5>
 
@@ -166,7 +167,7 @@
                                     <template x-for="(solution, index) in challenge.solutions">
                                         <li>
                                             <div
-                                                class="flex flex-col px-4 min-h-full w-full rounded-lg bg-content/5 border border-black/[0.15] border-t-2 border-t-primary">
+                                                class="flex flex-col px-4 min-h-full w-full rounded-lg bg-card dark:bg-content/5 border border-black/[0.15] border-t-2 border-t-primary">
 
                                                 <h3 class="mt-4 font-semibold" x-text="solution.title">
                                                 </h3>
@@ -218,10 +219,15 @@
                             src="https://images.unsplash.com/photo-1573164574511-73c773193279?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNjE2NXwwfDF8c2VhcmNofDR8fGFmcmljYW4lMjBhbWVyaWNhbiUyMGxlYWRlcnN8ZW58MHx8fHwxNzA5NjM2OTQwfDA&ixlib=rb-4.0.3&q=80&w=1080"
                             x-bind:src="challenge.image" alt="" />
 
-                        <svg class="h-full absolute z-10 -translate-x-3 scale-110 text-card" fill="currentColor"
+                        {{-- <svg class="h-full absolute z-10 -translate-x-3 scale-110 text-card" fill="currentColor"
                             viewBox="0 0 160.288 1440">
                             <path
                                 d="M 32 1440 L 48 1392 C 64 1344 96 1248 122.7 1152 C 149 1056 171 960 154.7 864 C 139 768 85 672 69.3 576 C 53 480 75 384 69.3 288 C 64 192 32 96 16 48 L 8.81745e-14 0 L 0 1440 Z" />
+                        </svg> --}}
+                        <svg class="h-full absolute z-10 -left-24 top-12s scale-105 -rotate-6s text-card"
+                            viewBox="0 0 687.881 2374.188" fill="currentColor">
+                            <path id="Union_2" data-name="Union 2"
+                                d="M320.441,2374.188c-33.1,0-60-145.745-60-325.083V1779.767c-5.363-24.2-8.238-52.447-7.759-82.417.816-52.135,2.884-104.205,7.161-154.908C123.157,1476.846,0,1131.013,0,779.313Q0,727.584,2.348,682.1A208.976,208.976,0,0,1,.581,635.063Q17.531,389.89,96.781,210.4,189.781-.128,340.881,0q159,0,253,213.227,94.049,213.227,94,496.247,0,156.649-34.6,296.362-34.652,140.1-147.8,380.527-58.5,124.7-72.7,200.4-3.791,19.975-6.44,48.416c.721,20.153,1.1,40.988,1.1,62.3V2049.1c0,179.338-26.9,325.083-60,325.083Z" />
                         </svg>
                     </div>
                 </div>
